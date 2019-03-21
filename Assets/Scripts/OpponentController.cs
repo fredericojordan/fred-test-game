dda0328d43f937dd07d5f113b2c4870aa2b48754
Frederico Jordan <fredericojordan@gmail.com>
2019-03-21 02:03:23 -0300

    Randomizing opponents' starting positions

--- Assets/Scripts/OpponentController.cs
+++ Assets/Scripts/OpponentController.cs
@@ -14,16 +14,17 @@ public class OpponentController : MonoBehaviour {
 	void Start()
 	{
 		rb = GetComponent<Rigidbody>();
+
+		float rand_x = -6.0f + (12.0f*Random.value);
+		float rand_z = -6.0f + (12.0f*Random.value);
+		rb.position = new Vector3(rand_x, 0.5f, rand_z);
 	}
 
     void FixedUpdate()
 	{
 		Vector3 movement = center - transform.position;
+		movement[1] = 0.0f;
 		rb.AddForce(movement * speed);
-
-		if (transform.position[1] <= -5.0F) {
-		    gameObject.SetActive(false);
-		}
 	}
 
 }
