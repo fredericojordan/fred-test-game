2641b6ce9c572c330d8784581f9dd9584242d6a0
Frederico Jordan <fredericojordan@gmail.com>
2019-03-21 01:43:42 -0300

    Ground shrink refactor

---
+++ Assets/Scripts/ShrinkAndDisappear.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShrinkAndDisappear : MonoBehaviour
+{
+    private const float shrink_speed = 10.0F;
+    private const float min_scale = 0.01F;
+
+    void Update()
+    {
+        float actual_speed = shrink_speed * 0.0001F;
+        transform.localScale -= new Vector3(actual_speed, 0, actual_speed);
+
+        if (transform.localScale[0] <= min_scale) {
+            gameObject.SetActive(false);
+        }
+    }
+}
