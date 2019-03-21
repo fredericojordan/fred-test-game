6093bd143b456af8c097e51285ce4d105492c2ac
Frederico Jordan <fredericojordan@gmail.com>
2019-03-21 01:27:57 -0300

    Adding kill text

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,10 +8,12 @@ public class PlayerController : MonoBehaviour {
 	
 	public float speed;
 	public Rigidbody rb;
+	public Text killText;
 
 	void Start()
 	{
 		rb = GetComponent<Rigidbody>();
+		killText.text = "";
 	}
 
 	void FixedUpdate()
@@ -22,5 +24,15 @@ public class PlayerController : MonoBehaviour {
 		Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
 
 		rb.AddForce(movement * speed);
+
+		if (transform.position[1] <= -3.0f) {
+		    Kill();
+		}
+	}
+
+	void Kill()
+	{
+        rb.isKinematic = true;
+		killText.text = "U DED MA FREND";
 	}
 }
